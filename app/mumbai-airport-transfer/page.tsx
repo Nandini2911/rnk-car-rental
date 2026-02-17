abe67386fb3864f78c30bf677136814e29718921
Nandini yadav <nandaniyadav521@gmail.com>
2026-02-17 18:19:49 +0530

    updated

--- app/mumbai-airport-transfer/page.tsx
+++ app/mumbai-airport-transfer/page.tsx
@@ -1,5 +1,22 @@
+import { RnkFooter } from "@/components/footer";
+import { NavBar } from "@/components/NavBar";
 import RNKLandingPage from "@/components/RNKLandingPage";
 
+export const metadata = {
+  title: "Luxury Airport Transfer in Mumbai | RNK",
+  description:
+    "Premium chauffeur driven Mercedes & BMW airport transfer in Mumbai. Available 24/7 for T1 & T2.",
+};
+
 export default function Page() {
-  return <RNKLandingPage />;
+
+    <NavBar/>
+  return (
+    <>
+      <RNKLandingPage />
+      <RnkFooter />
+    </>
+  );
+
+
 }
